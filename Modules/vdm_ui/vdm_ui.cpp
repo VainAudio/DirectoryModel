0758f05f51604d1f1eb365eb6d67421cea745063
Rami Hansen <rami.hansen2@gmail.com>
2026-07-21 15:05:50 -0700

    #29 organize juce tree view into its own dir

--- Modules/vdm_ui/vdm_ui.cpp
+++ Modules/vdm_ui/vdm_ui.cpp
@@ -1,8 +1,8 @@
-#include "TreeView/vdm_JuceTreeView.cpp"
 #include "TreeView/vdm_TreeView.cpp"
 #include "TreeView/vdm_TreeViewItemBase.cpp"
 #include "TreeView/vdm_TreeViewItemDefault.cpp"
-#include "TreeView/vdm_JuceTreeViewItem.cpp"
-#include "TreeView/vdm_JuceTreeViewItemComponent.cpp"
-
 #include "TreeView/vdm_TreeViewSelectorKeyListener.cpp"
+
+#include "JuceTreeView/vdm_JuceTreeView.cpp"
+#include "JuceTreeView/vdm_JuceTreeViewItem.cpp"
+#include "JuceTreeView/vdm_JuceTreeViewItemComponent.cpp"
